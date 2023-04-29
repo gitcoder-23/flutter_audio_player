7f549e789cd55e140c4ad1c9f10dbe0f23328c06
prabhat-cn <prabhat@capitalnumbers.com>
2023-04-29 12:00:42 +0530

    tools service issue fixed by "xmlns:tools="http://schemas.android.com/tools""

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_audio_player/pages/singleaudio/SingleAudioPlayerPage.dart';
+import 'package:just_audio_background/just_audio_background.dart';
 
 Future<void> main() async {
-  // await JustAudioBackground.init(
-  //   androidNotificationChannelId: 'com.ryanheise.bg_demo.channel.audio',
-  //   androidNotificationChannelName: 'Audio playback',
-  //   androidNotificationOngoing: true,
-  // );
+  await JustAudioBackground.init(
+    androidNotificationChannelId: 'com.ryanheise.bg_demo.channel.audio',
+    androidNotificationChannelName: 'Audio playback',
+    androidNotificationOngoing: true,
+    // androidStopForegroundOnPause: true,
+    // fastForwardInterval: const Duration(seconds: 10),
+    // rewindInterval: const Duration(seconds: 10),
+  );
   runApp(const MyApp());
 }
 
